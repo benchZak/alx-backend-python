147e89b8277bb3066264d8d99b3a2023a83dd6ef
benchzak <benchzakaria3@gmail.com>
2025-07-20 16:28:36 +0100

    2. Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
+from rest_framework.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'password']
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -12,17 +13,27 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class MessageSerializer(serializers.ModelSerializer):
+    message_body = serializers.CharField()  # Explicit use of CharField
     sender = UserSerializer(read_only=True)
 
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+
+    def validate_message_body(self, value):
+        if not value.strip():
+            raise ValidationError("Message body cannot be empty.")
+        return value
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    message_count = serializers.SerializerMethodField()  # Adds method field
 
     class Meta:
         model = Conversation
-        fields = ['id', 'participants', 'created_at', 'messages']
+        fields = ['conversation_id', 'participants', 'created_at', 'messages', 'message_count']
+
+    def get_message_count(self, obj):
+        return obj.messages.count()
 
